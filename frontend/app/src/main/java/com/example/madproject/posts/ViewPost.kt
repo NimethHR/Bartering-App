fdbf82720c0c8307c3522a4593a0df63e3cd32b5
IT21294648 <c.wick400@gmail.com>
2023-03-19 15:10:02 +0530

    individual post view

---
+++ frontend/app/src/main/java/com/example/madproject/posts/ViewPost.kt
@@ -0,0 +1,12 @@
+package com.example.madproject.posts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madproject.R
+
+class ViewPost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_post)
+    }
+}
