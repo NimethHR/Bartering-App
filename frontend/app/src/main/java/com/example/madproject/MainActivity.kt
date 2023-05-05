c25993d60f467e175a301bf6a66d73363c197d2e
ravii-sa <ravinduhapuarachchi@gmail.com>
2023-05-05 21:57:06 +0530

    use fragments

--- frontend/app/src/main/java/com/example/madproject/MainActivity.kt
+++ frontend/app/src/main/java/com/example/madproject/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.madproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madproject.fragments.GroupListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_bar)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.bottom_nav_bar_groups -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, GroupListFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val auth = Firebase.auth
+
+        auth.signInWithEmailAndPassword("test2@gmail.com", "password")
+
+
     }
 }
---
+++ frontend/app/src/main/java/com/example/madproject/fragments/GroupInfoFragment.kt
@@ -0,0 +1,118 @@
+package com.example.madproject.fragments
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.madproject.R
+import com.example.madproject.databinding.FragmentGroupInfoBinding
+import com.example.madproject.util.DatabaseFunctions
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM3 = "param3"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GroupInfoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GroupInfoFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var id: String? = null
+    private var name: String? = null
+    private var description: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            id = it.getString(ARG_PARAM1)
+            name = it.getString(ARG_PARAM2)
+            description = it.getString(ARG_PARAM3)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val binding: FragmentGroupInfoBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_group_info, container, false)
+
+        binding.groupInfoToolbar.title = name
+        binding.tvGroupViewName.text = name
+        binding.tvGroupViewDescription.text = description
+
+        binding.groupInfoToolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.group_info_delete -> {
+                    val alertDialog = AlertDialog.Builder(context)
+                        .setTitle("Confirm Delete")
+                        .setMessage("Are you sure you want to delete this group?")
+                        .setPositiveButton("Yes") { _, _ ->
+                            DatabaseFunctions.deleteGroup(id!!){ success ->
+                                if (!success) {
+                                    return@deleteGroup
+                                }
+
+                                val transaction = parentFragmentManager.beginTransaction()
+                                transaction.replace(R.id.fragment_container, GroupListFragment())
+                                transaction.addToBackStack(null)
+                                transaction.commit()
+                            }
+                        }
+                        .setNegativeButton("No", null)
+                        .create()
+                    alertDialog.show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.btnGroupViewInvite.setOnClickListener {
+            DatabaseFunctions.getUserByDisplayName(binding.etGroupViewInvite.text.toString()) { user ->
+                if (user == null) {
+                    binding.etGroupViewInvite.error = "User not found"
+                    return@getUserByDisplayName
+                }
+
+                val currentUser = DatabaseFunctions.getCurrentUser()
+
+                DatabaseFunctions.inviteUserToGroup(id!!, name!!, user.id!!, currentUser?.uid!!, currentUser.displayName ?: currentUser.email!!) { success ->
+
+                    if (!success) {
+                        return@inviteUserToGroup
+                    }
+
+                    binding.etGroupViewInvite.text?.clear()
+                }
+            }
+        }
+
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment GroupInfoFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic fun newInstance(id: String?, name: String?, description: String?) =
+                GroupInfoFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, id)
+                        putString(ARG_PARAM2, name)
+                        putString(ARG_PARAM3, description)
+                    }
+                }
+    }
+}
