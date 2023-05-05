8269560b610eebebeb51e622a7cb902e1017a9d6
IT21294648 <c.wick400@gmail.com>
2023-05-05 22:28:42 +0530

    temp changes

--- frontend/app/src/main/java/com/example/madproject/posts/CreatePost.kt
+++ frontend/app/src/main/java/com/example/madproject/posts/CreatePost.kt
@@ -2,11 +2,24 @@ package com.example.madproject.posts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import com.example.madproject.R
+import com.example.madproject.databinding.ActivityCreatePostBinding
 
 class CreatePost : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreatePostBinding
+
+    private lateinit var create_post_title: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_post)
+
+        create_post_title = findViewById(R.id.create_post_title)
+
     }
 }
