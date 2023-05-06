81fcb61fadf8b80a814d96e0f27849ef62769f00
it21294198 <it21294198@my.sliit.lk>
2023-05-06 17:28:34 +0530

    ok

---
+++ frontend/app/src/main/java/com/example/madproject/admin_dashboard/AddFeedback.kt
@@ -0,0 +1,39 @@
+package com.example.test.admin_dashboard
+
+import android.content.Intent
+import android.net.Uri
+import com.example.test.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+
+class AddFeedback : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    private val pickImage = 100
+    private var imageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_feedback)
+
+        title = "KotlinApp"
+        imageView = findViewById(R.id.imageView)
+        button = findViewById(R.id.buttonLoadPicture)
+        button.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            imageView.setImageURI(imageUri)
+        }
+    }
+}
