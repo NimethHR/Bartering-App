82848d66f7bd7d4d506bb659146a870efdbe416a
it21294198 <it21294198@my.sliit.lk>
2023-05-06 17:27:47 +0530

    ok

--- frontend/app/src/main/java/com/example/madproject/MainActivity.kt
+++ frontend/app/src/main/java/com/example/madproject/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.madproject
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.test.admin_dashboard.Dashboard
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.button)
+        var dash = findViewById<Button>(R.id.btn_dash)
+
+        val db = Firebase.firestore
+        btn.setOnClickListener {
+
+            val user = hashMapOf(
+                "first" to "Ada",
+                "last" to "Lovelace",
+                "born" to 1815
+            )
+
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(ContentValues.TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(ContentValues.TAG, "Error adding document", e)
+                }
+        }
+
+        dash.setOnClickListener {
+            var intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+        }
+
     }
 }
