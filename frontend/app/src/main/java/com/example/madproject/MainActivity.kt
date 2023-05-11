7696a8093b89b40b57a1365c3b76b5dd7e5dc21c
Nimeth <nrh.herath@gmail.com>
2023-05-11 08:55:16 +0530

    bug fix

--- frontend/app/src/main/java/com/example/madproject/MainActivity.kt
+++ frontend/app/src/main/java/com/example/madproject/MainActivity.kt
@@ -14,9 +14,7 @@ import com.google.firebase.auth.ktx.auth
 
 import android.util.Log
 import android.widget.Button
-import com.example.madproject.fragments.CreatePostFragment
 import com.example.madproject.fragments.GroupListFragment
-import com.example.madproject.fragments.HomePage
 
 import com.google.firebase.ktx.Firebase
 
@@ -42,11 +40,9 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.bottom_nav_bar_post -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, CreatePostFragment()).commit()
                     true
                 }
                 R.id.bottom_nav_bar_home -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomePage()).commit()
                     true
                 }
                 else -> false
